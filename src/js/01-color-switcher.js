2b5d9c05a90ea24155b4ea95df44e894a185b232
WiktorSi <vitek2@poczta.onet.pl>
2023-03-11 20:10:18 +0100

    HO-09 part III

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.querySelector("body");
+;
+
+const changeColor = () => {
+    colorId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startButton.disabled = true;
+    stopButton.disabled = false
+};
+
+const stopChange = () => {
+    clearInterval(colorId);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+} 
+
+startButton.addEventListener("click", changeColor);
+stopButton.addEventListener("click", stopChange);
